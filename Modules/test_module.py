eef5fc3f2ad73240a76721b0224af2c3326ed71c
Just-A-Mango <horace.hoff@gmail.com>
2021-12-04 14:46:40 +0100

    Added comments

--- Modules/test_module.py
+++ Modules/test_module.py
@@ -1,6 +1,7 @@
+#Put all your needed imports here ('os' is required)
 import os
 
-
+#Main function
 def main(input):
     #Put all your code/actions here
     input = input.replace("x","")
@@ -9,7 +10,7 @@ def main(input):
 
 
 
-
+#Code used to communicate with the main file
 output = ""
 with open(os.getcwd() + '\\Modules\\input.txt', 'r') as f:
     output = f.readlines()[0]
