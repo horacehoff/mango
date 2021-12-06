4c6097ac581198d4784c3bb58cf09ead169c028a
Just-A-Mango <horace.hoff@gmail.com>
2021-12-06 19:17:01 +0100

    Created a beautiful windows 11-like GUI installer! You can still install Fox via the cute little CLI one though :)

---
+++ Installer/cli-fox-downloader.py
@@ -0,0 +1,67 @@
+def ask_forfolder():
+    import tkinter as tk
+    from tkinter import filedialog
+
+    from tkinter import Tk, filedialog
+
+    root = Tk()
+    root.withdraw()
+
+    root.attributes('-topmost', True)
+
+    open_file = filedialog.askdirectory()
+    return open_file
+
+def install_module(module):
+    try:
+        import subprocess, os
+        subprocess.call(["pip", "install", module], stdout = open(os.devnull, "w"), stderr = subprocess.STDOUT)
+    except:
+        pass
+    
+def fail():
+    import colored
+    print(colored.stylize("Fox installation failed. Please try again", colored.fg("red")))
+
+try:
+    from rich.progress import Progress
+except:
+    try:
+        install_module('rich')
+    except:
+        fail()
+
+with Progress() as progress:
+
+    task1 = progress.add_task("[bold red]Installing Fox...", total=1000)
+
+    while not progress.finished:
+        try:
+            import colored
+        except:
+            try:
+                install_module('colored')
+            except:
+                fail()
+        try:
+            import requests
+        except:
+            try:
+                install_module('requests')
+            except:
+                fail()
+        progress.update(task1, advance = 333)
+        url = "https://raw.githubusercontent.com/Just-A-Mango/fox/main/fox.py"
+        r = requests.get(url, allow_redirects=True)
+        progress.update(task1, advance = 333)
+        folder = ask_forfolder()
+        try:
+            r = requests.get(url, allow_redirects=True)
+            open(folder+'\\fox.py', 'wb').write(r.content)
+        except:
+            import ctypes
+            ctypes.windll.user32.MessageBoxW(0, "Fox installation failed.", "F A I L", 1)
+        progress.update(task1, advance = 334)
+        progress.stop()
+        import rich
+        rich.print("👍 [bold red]Successfully installed Fox![/bold red] 👍")
---
+++ Installer/fox_downloader.py
@@ -0,0 +1,68 @@
+from ctypes import sizeof
+import tkinter as tk
+from tkinter import ttk
+from tkinter.ttk import Label
+
+
+def ask_forfolder():
+    from tkinter import filedialog
+
+    from tkinter import Tk, filedialog
+
+    root = Tk()
+    root.withdraw()
+
+    root.attributes('-topmost', True)
+
+    open_file = filedialog.askdirectory()
+    return open_file
+
+def download_and_install():
+    try:
+        import requests
+        url = "https://raw.githubusercontent.com/Just-A-Mango/fox/main/fox.py"
+        gif_url = 'https://media.giphy.com/media/3ornk57KwDXf81rjWM/giphy.gif'
+        folder = ask_forfolder()
+        r = requests.get(url, allow_redirects=True)
+        open(folder+'\\fox.py', 'wb').write(r.content)
+        label = Label(window, text='Successfully downloaded and installed Fox! 😀')
+        label.pack()
+        import ctypes  # An included library with Python install.
+        ctypes.windll.user32.MessageBoxW(0, "Fox was successfully installed 😀!", "SUCCESS!", 0)
+        window.quit()
+    except:
+        import ctypes
+        ctypes.windll.user32.MessageBoxW(0, "Fox installation failed.", "F A I L", 1)
+        
+
+window = tk.Tk()
+window.title("Fox Installer")
+window.geometry("250x200")
+window.tk.call("source", "sun-valley.tcl")
+window.tk.call("set_theme", "dark")
+window.style = ttk.Style()
+MyLab = Label(window, text="FOX", font=("Poppins",50))
+MyLab.pack()
+import time
+time.sleep(1)
+MyLab['text'] = "FOX"
+button = ttk.Button(
+    text="Download Fox",
+    command=lambda: download_and_install(),
+    style="Accent.TButton"
+    )
+button.pack(pady=40)
+window.update()
+import time
+MyLab['text'] = "</*"
+window.update()
+time.sleep(0.22)
+MyLab['text'] = "<O*"
+window.update()
+time.sleep(0.22)
+MyLab['text'] = ">OX"
+window.update()
+time.sleep(0.22)
+MyLab['text'] = "FOX"
+window.update()
+window.mainloop()
